68ea30d1152723de0be94d7c160a24828a398601
Mahid Wasif <mahidwasif2004@gmail.com>
2026-06-12 18:12:31 +0500

    minor changes

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -43,10 +43,10 @@ function Footer() {
                 <div>
                     <h3 className="font-semibold mb-4">Support</h3>
                     <ul className="space-y-2 text-gray-400">
-                        <li className="hover:text-white cursor-pointer">Help Center</li>
-                        <li className="hover:text-white cursor-pointer">Terms of Service</li>
-                        <li className="hover:text-white cursor-pointer">Privacy Policy</li>
-                        <li className="hover:text-white cursor-pointer">Contact Us</li>
+                        <li className="hover:text-white cursor-pointer"> <a href="/"> Help Center </a></li>
+                        <li className="hover:text-white cursor-pointer"> <a href="/">Terms of Service</a></li>
+                        <li className="hover:text-white cursor-pointer"> <a href="/">Privacy Policy</a></li>
+                        <li className="hover:text-white cursor-pointer"> <a href="/">Contact Us</a></li>
                     </ul>
                 </div>
 
@@ -54,15 +54,15 @@ function Footer() {
                 <div>
                     <h3 className="font-semibold mb-4">Follow Us</h3>
                     <div className="flex gap-4">
-                        <a className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 hover:scale-110 transition" href="#">
+                        <a className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-600 hover:scale-110 transition" href="/">
                             <FaFacebookF />
                         </a>
 
-                        <a className="w-10 h-10 flex items-center justify-center rounded-full bg-pink-500 hover:scale-110 transition" href="#">
+                        <a className="w-10 h-10 flex items-center justify-center rounded-full bg-pink-500 hover:scale-110 transition" href="https://www.instagram.com/cinee_ai?igsh=a3g0YWxiM2txbnlv&utm_source=qr">
                             <FaInstagram />
                         </a>
 
-                        <a className="w-10 h-10 flex items-center justify-center rounded-full bg-sky-500 hover:scale-110 transition" href="#">
+                        <a className="w-10 h-10 flex items-center justify-center rounded-full bg-sky-500 hover:scale-110 transition" href="/">
                             <FaTwitter />
                         </a>
                     </div>
